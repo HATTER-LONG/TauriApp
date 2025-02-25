06dc01143f31b672bf1b1cdffb39f57324e05cc0
caolei <caolei6767@gmail.com>
2025-02-25 17:39:12 +0800

    clean all file

--- types/pdfjs-dist.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'pdfjs-dist/build/pdf.worker.mjs' {
-  const content: unknown
-  export default content
-}
-declare module 'pdfjs-dist/web/pdf_viewer' {
-  export * from 'pdfjs-dist/types/web/pdf_viewer.component'
-  // const content: unknown
-  // export default content
-}
